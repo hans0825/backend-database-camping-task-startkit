b7f526c3df4f5c775ee09b3c32cc5cc570591468
hans <renohans7@gmail.com>
2024-12-08 13:46:30 +0800

    更新 6-2 題目

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -390,6 +390,13 @@ ORDER BY "COACH".experience_years desc;
 
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
+select 
+  "SKILL".name as "專長名稱", count(*) as coach_total
+FROM "COACH_LINK_SKILL"
+INNER JOIN "SKILL" on "SKILL".id = "COACH_LINK_SKILL".skill_id
+GROUP BY "SKILL".name 
+ORDER BY coach_total desc 
+LIMIT 1;
 
 -- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
