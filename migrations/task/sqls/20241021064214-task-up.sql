0400480747a9e557fd2f79b3eb8c8f47f308048e
hans <renohans7@gmail.com>
2024-12-08 08:17:37 +0800

    更新 1-5 題目

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -43,7 +43,7 @@ SELECT Count(*) AS "用戶數量"
 FROM "USER";
 
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
-
+SELECT * FROM "USER" LIMIT 3;
 
 --  ████████  █████   █    ████  
 --    █ █   ██    █  █         █ 
