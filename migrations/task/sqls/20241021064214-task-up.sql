7e2a0f7d5999f46a5350dd2711dcac256a7c56fc
hans <renohans7@gmail.com>
2024-12-08 13:33:19 +0800

    更新 6-1 題目

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -377,6 +377,16 @@ FROM (SELECT user_id, SUM(purchased_credits) AS total_credit
 -- 6. 後台報表
 -- 6-1 查詢：查詢專長為重訓的教練，並按經驗年數排序，由資深到資淺（需使用 inner join 與 order by 語法)
 -- 顯示須包含以下欄位： 教練名稱 , 經驗年數, 專長名稱
+SELECT 
+  "USER"."name" as "教練名稱",
+  "COACH".experience_years as "經驗年數", 
+  "SKILL".name as "專長名稱" 
+FROM "COACH_LINK_SKILL"
+INNER JOIN "SKILL" on "SKILL".id = "COACH_LINK_SKILL".skill_id
+INNER JOIN "COACH" on "COACH".id = "COACH_LINK_SKILL".coach_id
+INNER JOIN "USER" on "USER".id = "COACH".user_id
+WHERE "COACH_LINK_SKILL".skill_id = (select id from "SKILL" where name = '重訓')   
+ORDER BY "COACH".experience_years desc;
 
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
