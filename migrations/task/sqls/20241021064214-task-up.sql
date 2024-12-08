29b13f2279739df005d6c0330d7b7ab9025702ba
hans <renohans7@gmail.com>
2024-12-08 09:34:17 +0800

    更新 3-2 題目

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -113,6 +113,42 @@ WHERE "email" IN (
     -- 1. 所有教練都有 `重訓` 專長
     -- 2. 教練`肌肉棒子` 需要有 `瑜伽` 專長
     -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+VALUES 
+	(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io')),
+    (select id from "SKILL" where name = '重訓')
+	);
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+VALUES 
+	(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io')),
+    (select id from "SKILL" where name = '重訓')
+	);
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+VALUES 
+	(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
+    (select id from "SKILL" where name = '重訓')
+	);
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+VALUES 
+	(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io')),
+    (select id from "SKILL" where name = '瑜伽')
+	);
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+VALUES 
+	(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
+    (select id from "SKILL" where name = '有氧運動')
+	);
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+VALUES 
+	(
+    (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
+    (select id from "SKILL" where name = '復健訓練')
+	);
 
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
